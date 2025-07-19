9ab93a460c71f15d3ddeb266c9bfe1f04c0cef9e
nkp-2001 <83334420+nkp-2001@users.noreply.github.com>
2025-07-19 17:41:37 +0200

    add/fix out zoom in Dungeon Phase

--- DungeonDuell/Assets/DungeonDuell/Script/Gym/GymToCardTransition.cs
+++ DungeonDuell/Assets/DungeonDuell/Script/Gym/GymToCardTransition.cs
@@ -2,26 +2,29 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using DG.Tweening;
 using Cinemachine;
+using MoreMountains.TopDownEngine;
 
 namespace dungeonduell
 {
     public class GymToCardTransition : MonoBehaviour
     {
-        [Header("Camera References")]
-        [SerializeField] private CinemachineVirtualCamera player1Cam;
+        [Header("Camera References")] [SerializeField]
+        private CinemachineVirtualCamera player1Cam;
+
         [SerializeField] private CinemachineVirtualCamera player2Cam;
 
-        [Header("UI Overlay")]
-        [SerializeField] private CanvasGroup fadeCanvasGroup;
+        [Header("UI Overlay")] [SerializeField]
+        private CanvasGroup fadeCanvasGroup;
 
-        [Header("Settings")]
-        [SerializeField] private float targetZoom = 8f;
+        [Header("Settings")] [SerializeField] private float targetZoom = 8f;
         [SerializeField] private float zoomDuration = 1f;
         [SerializeField] private float fadeDuration = 0.6f;
         [SerializeField] private float fadeDelay = 0.7f;
         [SerializeField] private int targetSceneIndex = 2;
 
         private bool transitionStarted = false;
+
+        [SerializeField] bool getCamSelf = false;
 
         private void OnEnable()
         {
@@ -51,21 +54,34 @@ namespace dungeonduell
             if (transitionStarted) return;
             transitionStarted = true;
 
+            if (getCamSelf)
+            {
+                CinemachineCameraController[] cams =
+                    FindObjectsByType<CinemachineCameraController>(FindObjectsSortMode.None);
+
+                player1Cam = cams[0].GetComponent<CinemachineVirtualCamera>();
+                player2Cam = cams[1].GetComponent<CinemachineVirtualCamera>();
+                Animator camAni1 = player1Cam.GetComponent<Animator>();
+                Animator camAni2 = player1Cam.GetComponent<Animator>();
+                if (camAni1 != null) camAni1.enabled = false;
+                if (camAni2 != null) camAni2.enabled = false;
+            }
+
             // Zoom beide Kameras
             if (player1Cam != null)
             {
                 DOTween.To(() => player1Cam.m_Lens.OrthographicSize,
-                           x => player1Cam.m_Lens.OrthographicSize = x,
-                           targetZoom,
-                           zoomDuration).SetEase(Ease.InOutCubic);
+                    x => player1Cam.m_Lens.OrthographicSize = x,
+                    targetZoom,
+                    zoomDuration).SetEase(Ease.InOutCubic);
             }
 
             if (player2Cam != null)
             {
                 DOTween.To(() => player2Cam.m_Lens.OrthographicSize,
-                           x => player2Cam.m_Lens.OrthographicSize = x,
-                           targetZoom,
-                           zoomDuration).SetEase(Ease.InOutCubic);
+                    x => player2Cam.m_Lens.OrthographicSize = x,
+                    targetZoom,
+                    zoomDuration).SetEase(Ease.InOutCubic);
             }
 
             // Fading
@@ -73,10 +89,7 @@ namespace dungeonduell
             {
                 fadeCanvasGroup.DOFade(1f, fadeDuration)
                     .SetDelay(fadeDelay)
-                    .OnComplete(() =>
-                    {
-                        SceneManager.LoadScene(targetSceneIndex);
-                    });
+                    .OnComplete(() => { SceneManager.LoadScene(targetSceneIndex); });
             }
             else
             {
