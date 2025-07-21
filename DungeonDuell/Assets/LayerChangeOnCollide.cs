49495f673953e8b5a5587d39e62622cd03a2edb3
nkp-2001 <83334420+nkp-2001@users.noreply.github.com>
2025-07-21 14:12:00 +0200

    make big crystal deko object change layer so you not under its root or on it.

---
+++ DungeonDuell/Assets/LayerChangeOnCollide.cs
@@ -0,0 +1,27 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace dungeonduell
+{
+    public class LayerChangeOnCollide : MonoBehaviour
+    {
+        public int layerOnCollide = 100;
+        public int layerOffCollide = 0;
+        private SpriteRenderer _sprite;
+
+        void Start()
+        {
+            _sprite = GetComponent<SpriteRenderer>();
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            _sprite.sortingOrder = layerOnCollide;
+        }
+
+        void OnTriggerExit2D(Collider2D other)
+        {
+            _sprite.sortingOrder = layerOffCollide;
+        }
+    }
+}
