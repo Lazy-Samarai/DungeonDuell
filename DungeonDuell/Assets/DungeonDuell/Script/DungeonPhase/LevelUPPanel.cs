59ada893bab8b54076f57edadd5ea93f72ab1fac
Vinzzman <vincentvonhartz@gmx.de>
2025-07-23 14:43:02 +0200

    [EDIT] Feedback for Upgrades

--- DungeonDuell/Assets/DungeonDuell/Script/DungeonPhase/LevelUPPanel.cs
+++ DungeonDuell/Assets/DungeonDuell/Script/DungeonPhase/LevelUPPanel.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 using UnityEngine.Serialization;
 using FMODUnity;
 using Unity.VisualScripting;
+using System.Collections;
 
 namespace dungeonduell
 {
     public class LevelUpPanel : MonoBehaviour, IObserver
     {
         private const int AmountPerUpgrade = 1;
-        [FormerlySerializedAs("LevelUpMenu")] public GameObject levelUpMenu; // Das Panel für das Level-Up
-        [FormerlySerializedAs("TestHub")] public TestHub testHub;
+
+        [FormerlySerializedAs("LevelUpMenu")] 
+        public GameObject levelUpMenu; // Das Panel für das Level-Up
+        
+        [FormerlySerializedAs("TestHub")] 
+        public TestHub testHub;
+
         public bool player1;
+
         private DungeonDuellMultiplayerLevelManager _levelManager;
-        [Header("UI Elements")] private bool _menuOpen;
+        [Header("UI Elements")] 
+        private bool _menuOpen;
 
         [SerializeField] private EventReference openLevelUPEvent;
         [SerializeField] private EventReference closeLevelUpEvent;
         [SerializeField] private EventReference optionSelectedEvent;
+
+        [Header("Upgrade Feedback Targets")]
+        [SerializeField] private UpgradeFeedback speedFeedback;
+        [SerializeField] private UpgradeFeedback attackSpeedFeedback;
+        [SerializeField] private UpgradeFeedback healFeedback;
 
         private void Start()
         {
@@ -28,16 +41,31 @@ namespace dungeonduell
         public void UpgradeAttackSpeed()
         {
             OnOptionSelected(LevelUpOptions.AttackSpeed);
+            // Hier soll das UpgradeFeedback hin
+            if (attackSpeedFeedback != null)
+            {
+                attackSpeedFeedback.PlayFeedback();
+            }
         }
 
         public void UpgradeSpeedd()
         {
             OnOptionSelected(LevelUpOptions.Speed);
+            // Hier soll das UpgradeFeedback hin
+            if (speedFeedback != null)
+            {
+                speedFeedback.PlayFeedback();
+            }
         }
 
         public void UpgradeHealth()
         {
             OnOptionSelected(LevelUpOptions.HealingInstead);
+            // Hier soll das UpgradeFeedback hin
+            if (healFeedback != null)
+            {
+                healFeedback.PlayFeedback();
+            }
         }
 
         public void ShowLevelUpMenu(bool on)
@@ -45,7 +73,7 @@ namespace dungeonduell
             _menuOpen = on;
             levelUpMenu.SetActive(on);
 
-            if (on == true)
+            if (on)
             {
                 RuntimeManager.PlayOneShot(openLevelUPEvent);
             }
@@ -57,15 +85,25 @@ namespace dungeonduell
 
         private void OnOptionSelected(LevelUpOptions option)
         {
-            if (testHub.canLevelUp & _menuOpen)
+            if (testHub.canLevelUp && _menuOpen)
             {
+                // (Optional: Hier könnte man den EventSystem-Ansatz ganz entfernen.)
+
                 if (_levelManager != null)
+                {
                     _levelManager.ApplyLevelUpPerCoins(option, AmountPerUpgrade, player1 ? 1 : 2);
+                }
 
                 RuntimeManager.PlayOneShot(optionSelectedEvent);
                 testHub.menuShowing = false;
-                ShowLevelUpMenu(false);
+                StartCoroutine(CloseMenuWithDelay(0.5f));
             }
+        }
+
+        private IEnumerator CloseMenuWithDelay(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            ShowLevelUpMenu(false);
         }
 
         void OnEnable()
